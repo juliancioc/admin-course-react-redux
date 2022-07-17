657197bcb9bcd432cf55dc541cc4a641b89096ea
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-07-16 23:13:24 -0300

    feat: manage course page

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import { loadCourses } from '../../redux/actions/courseActions'
+import { loadAuthors } from '../../redux/actions/authorActions'
+
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert('Loading courses failed' + error)
+      })
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert('Loading authors failed' + error)
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  )
+}
+
+ManageCoursePage.propTypes = {
+  loadCourses: PropTypes.object.isRequired,
+  loadAuthors: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+}
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  }
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage)
